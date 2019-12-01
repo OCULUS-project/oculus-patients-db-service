452deed137fe7c4fc047751696be7da2daf4a954
jakubriegel <jakubriegel112@gmail.com>
2019-12-01 13:50:18 +0100

    component scan fix

--- src/main/kotlin/pl/poznan/put/oculus/oculuspatientsdbservice/OculusPatientsDbServiceApplication.kt
+++ src/main/kotlin/pl/poznan/put/oculus/oculuspatientsdbservice/OculusPatientsDbServiceApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("pl.poznan.put.oculus.boot", "")
+@ComponentScan("pl.poznan.put.oculus")
 class OculusPatientsDbServiceApplication
 
 fun main(args: Array<String>) {
